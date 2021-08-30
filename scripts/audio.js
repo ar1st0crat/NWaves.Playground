c3298bb018b71238ff0801d228ccd35ff42fe2da
ar1st0crat <art1st.prog@gmail.com>
2021-08-30 20:16:21 +0000

    Deploying to gh-pages from  @ 50f13c58295249185f7dfaddd13b9a1d40aec3a8 🚀

---
+++ scripts/audio.js
@@ -0,0 +1,20 @@
+﻿function base64ToArrayBuffer(base64) {
+    let binaryString = window.atob(base64);
+    let binaryLen = binaryString.length;
+    let bytes = new Uint8Array(binaryLen);
+    for (let i = 0; i < binaryLen; i++) {
+        let ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+    }
+    return bytes;
+}
+
+function loadAudio(data, containerId) {
+    let bytes = base64ToArrayBuffer(data);
+    let blob = new Blob([bytes], { type: 'audio/wav' });
+    let url = window.URL.createObjectURL(blob);
+    let audio = document.createElement('audio');
+    audio.controls = true;
+    audio.src = url;
+    document.getElementById(containerId).appendChild(audio);
+}
